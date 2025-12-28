f25710e06b79a6480ee17c1d10d76617b896316b
j4rviscmd <j4rviscmd@gmail.com>
2025-12-28 16:30:22 +0900

    added packages

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
+import streamlit as st
+
+
 def main():
-    print("Hello, World!")
+    st.set_page_config(page_title="Hello World App", layout="wide")
+    st.title("Hello, World App")
 
 
 if __name__ == "__main__":
