af9c62f3e260c4bb2c1768e559016d1ace40a297
j4rviscmd <j4rviscmd@gmail.com>
2025-12-28 16:37:35 +0900

    描画

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 import streamlit as st
 
+from components.advanced_dataframe.my_component import (
+    my_component as advanced_dataframe,
+)
+
 
 def main():
-    st.set_page_config(page_title="Hello World App", layout="wide")
-    st.title("Hello, World App")
+    TITLE = "Sample App"
+    st.set_page_config(page_title=TITLE, layout="wide")
+    st.title(TITLE)
+    advanced_dataframe("piyo")
 
 
 if __name__ == "__main__":
